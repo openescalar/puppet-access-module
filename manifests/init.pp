aec81a64f69000d8fdcdda1d2d214feb7d7d7103
Miguel Zuniga <miguelzuniga@gmail.com>
2014-01-08 23:19:25 -0700

    Upated email address agian

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,4 @@
-# Copyright 2014 Miguel Zuniga <miguel-zuniga at hotmail.com>
+# Copyright 2014 Miguel Zuniga ( miguel-zuniga at hotmail.com )
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
